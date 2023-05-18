448b60f2fea47f886e334ce91d764c87c4a1f170
GEORGE W RATCLIFFE <george70fs@outlook.com>
2023-05-18 15:54:38 +0100

    tidy up dates

---
+++ BlazorBooking/Client/wwwroot/js/JavaScript.js
@@ -0,0 +1,13 @@
+﻿(function () {
+
+    window.setdates = () => {
+
+        $('table tr td:nth-child(3)').each(
+            function () {
+                var td = $(this)
+                td.text(td.text().substring(0,9))
+            }
+        )
+    }
+
+})();
