532edc445fdf8faa364256b280be0f93ae46c90b
GEORGE W RATCLIFFE <george70fs@outlook.com>
2023-05-18 16:10:51 +0100

    dates tidyup

--- BlazorBooking/Client/wwwroot/js/JavaScript.js
+++ BlazorBooking/Client/wwwroot/js/JavaScript.js
@@ -5,7 +5,14 @@
         $('table tr td:nth-child(3)').each(
             function () {
                 var td = $(this)
-                td.text(td.text().substring(0,9))
+
+                //var date = new Date(td.text().substring(0, 9));
+                //var newDate = date.toString('dd-MM-yy');
+                var dateAr = td.text().substring(0, 9).split('/')
+
+                var newDate = dateAr[1].slice(-2) + '/' + dateAr[0].slice(-2) + '/' + dateAr[2];
+                td.text(newDate)
+                //td.text(td.text().substring(0,9))
             }
         )
     }
